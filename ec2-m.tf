d9f759fd8933636dec1602243c7a9a98178de2af
unknown <lmatrix47@gmail.com>
2024-01-15 18:31:47 +0530

    adding a terraf module

---
+++ ec2-m.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {   			 
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]   
+  }
+
+}
+
+
+resource "aws_instance" "myec2" {
+   ami = data.aws_ami.app_ami.id
+   instance_type = var.insta_type
+   key_name = "cwdemo"
+ }
